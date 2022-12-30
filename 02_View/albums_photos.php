ccd8054c7b6308bda87f9786f233dd881d69a25a
TerevaMillet <59101444+TerevaM@users.noreply.github.com>
2022-12-30 19:38:33 +0100

    cocommit

--- 02_View/albums_photos.php
+++ 02_View/albums_photos.php
@@ -1,16 +1,18 @@
 <?php ob_start(); ?>
-<div class="container my-5 py-5 bg-secondary cards_heroes">
+<div class="container my-5 py-5">
     <div class="row d-flex justify-content-around ">
 <?php
 foreach($albums as $value) {
 ?>
 
-<div class="card col-4 m-1" style="width: 18rem; height:20rem">
-  <!-- <img src="..." class="card-img-top" alt="..."> -->
+<div class="card col-4 m-1" style="width: 18rem; height:auto">
+
   <div class="card-body position-relative">
     <h3 class="card-title bg-primary p-2 text-center text-white"><?= $value->getName(); ?></h3>
     <span class="text-uppercase">Category : <?= $value->getCategory(); ?></span><br>
-    <span>Attaque : <?= $value->getRelease_date(); ?></span><br>
+    <span>date de sortie : <?= $value->getRelease_date(); ?></span><br>
+    <img src="04_utils/pictures/pic1.jpg" class="img-fluid mb-5" alt="Responsive image">
+    <!-- btn edit & delete -->
     <div class="d-flex justify-content-around position-absolute fixed-bottom py-2">
         <a href="<?= URL ?>heros/edit/<?= $value->getId() ?>" class="btn btn-primary"><i class="fa-regular fa-pen-to-square"></i></a>
         <form action="<?= URL ?>heros/delete/<?= $value->getId() ?>" method="POST"
@@ -20,20 +22,18 @@ foreach($albums as $value) {
     </div>
   </div>
 </div>
-
-
 <?php
 }
-if(isset($_SESSION) && $_SESSION['rang'] == 'admin'):
+// if(isset($_SESSION) && $_SESSION['rang'] == 'admin'):
 ?>
 <div class="card col-4 m-1" style="width: 18rem;">
   <div class="card-body d-flex justify-content-center align-items-center flex-column">
-    <h5 class="card-title bg-primary p-2 text-center text-white">New Hero</h5>
-    <a class="btn btn-primary" href="<?= URL ?>heros/add">+</a>
+    <h5 class="card-title bg-primary p-2 text-center text-white">New Album</h5>
+    <a class="btn btn-primary" href="<?= URL ?>albums_photos/create"><i class="fa-duotone fa-plus"></i></a>
   </div>
 </div>
 <?php 
-endif;
+// endif;
 ?>
 </div>
 </div>
